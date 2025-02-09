3c41a99a94c9eef6b02d7ecf1e02991ccdc60431
My EVM <yo@myevm.org>
2025-02-09 08:26:26 +0000

    .

--- src/data/food/chicken.ts
+++ src/data/food/chicken.ts
@@ -18,7 +18,7 @@ export const chickenDishes: ChickenDish[] = [
       calories: "1550 cal",
       price: 13.79,
       includesSides: false,
-      img: "/public/chicken-alfredo.jpg",
+      img: "/chicken-alfredo.jpg",
       wrongPrices: [11.99, 12.49, 14.99],
       upsells: [],
       orderqs: []
@@ -29,7 +29,7 @@ export const chickenDishes: ChickenDish[] = [
       calories: "520 cal",
       price: 12.29,
       includesSides: true,
-      img: "/public/091624_June_LemonPepperChicken_Hero.jpg",
+      img: "/091624_June_LemonPepperChicken_Hero.jpg",
       wrongPrices: [10.99, 11.49, 13.49],
       upsells: [],
       orderqs: []
@@ -40,7 +40,7 @@ export const chickenDishes: ChickenDish[] = [
       calories: "1060 cal",
       price: 14.29,
       includesSides: true,
-      img: "/public/Cheddars_Q124__DijonChickenAndMushrooms_590x3.jpg",
+      img: "/Cheddars_Q124__DijonChickenAndMushrooms_590x3.jpg",
       wrongPrices: [12.99, 13.79, 15.49],
       upsells: [],
       orderqs: []
@@ -51,7 +51,7 @@ export const chickenDishes: ChickenDish[] = [
       calories: "550 cal",
       price: 13.99,
       includesSides: true,
-      img: "/public/key-west-chicken-and-shrimp-590x365.jpg",
+      img: "/key-west-chicken-and-shrimp-590x365.jpg",
       wrongPrices: [12.49, 13.29, 15.29],
       upsells: [],
       orderqs: []
@@ -62,7 +62,7 @@ export const chickenDishes: ChickenDish[] = [
       calories: "1050 - 1250 cal",
       price: 11.79,
       includesSides: true,
-      img: "/public/Cheddars_ChickenTendersPlatter_590x365.jpg",
+      img: "/Cheddars_ChickenTendersPlatter_590x365.jpg",
       wrongPrices: [9.99, 10.49, 12.29],
       upsells: [],
       orderqs: []
